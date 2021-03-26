2fdbdf302f7ed80bd151f2c97f5c7cc6eecfb3a9
Naima Ali <80524153+naima-ali@users.noreply.github.com>
2021-03-26 17:05:13 +0500

    Update random.php

--- public/random.php
+++ public/random.php
@@ -1,9 +1,18 @@
+<?php $randomInt = random_int(1, 10); ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Your lucky number</title>
-    </head>
-    <body>
-        <h1>Your lucky number is: <?php echo random_int(1, 10); ?></h1>
-    </body>
+<head>
+    <title>Your lucky number</title>
+</head>
+<body>
+<h1>Your lucky number is: <?php echo $randomInt; ?></h1>
+<?php if ($randomInt > 5) { ?>
+    <h2>Nice!</h2>
+<?php } ?>
+<p>
+    <a href="/kittens.php?number=<?php echo $randomInt; ?>">
+        Now show me <?php echo $randomInt; ?> kittens!
+    </a>
+</p>
+</body>
 </html>
